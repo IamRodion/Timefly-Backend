e440e1f96e71bc044be78db1fc258f6adfaea811
IamRodion <22568039+IamRodion@users.noreply.github.com>
2024-12-07 00:25:20 -0500

    Mejora el archivo secret_key_gen.py Hace que el archivo sea capaz de crear un archivo .env sí no existe, y añade la SECRET_KEY automáticamente

--- secret_key_gen.py
+++ secret_key_gen.py
@@ -3,6 +3,8 @@ import secrets
 def generate_secret_key():
     return ''.join(secrets.choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>/?') for i in range(50))
 
-# Generar una nueva clave y almacenarla en una variable
-secret_key = generate_secret_key()
-print(secret_key)
+# Genera una nueva clave y la almacena en una constante
+SECRET_KEY = generate_secret_key()
+# print(secret_key)
+with open('.env', mode='w+') as file:
+    file.write("SECRET_KEY = '{}'".format(SECRET_KEY))
